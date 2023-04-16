0afd4280f5e35b9e7dbd3951f65aa07f66d8f3a6
Noam Avitan <noamavitan159753@gmail.com>
2023-04-16 11:33:10 +0300

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,20 @@
 import React, {  useState } from "react";
 import { restaurants } from "../db";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
 
   return (
-   <div className="">
-    {restaurants && restaurants.map((restaurant) =>(
-      <div>
-      <h1>{restaurant.name}</h1>
+   <div className="grid grid-cols-4 gap-3 justify-items-center mx-auto my-0 container mt-10">
+    {restaurants && restaurants.map((restaurant,i) =>(
+      <div className="border p-2" key={i}>
+        <img className="w-[280px]" src={restaurant.images[1]}  alt="" />
+      <h1 className=" font-bold">{restaurant.name}</h1>
+      <div className="flex ">
+      <p className="">{restaurant.cuisine}</p>
+      <p className="ml-3">{restaurant.price}</p>
+      </div>
+      <NavLink className={"text-blue-600 text-2xl"}>Click for more details</NavLink>
       </div>
     ) )}
    </div>
