fa54acbfe87d0a281facf03518fb99e395ed30cc
Noam Avitan <noamavitan159753@gmail.com>
2023-04-18 19:12:11 +0300

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,7 @@ export default function Home() {
       <NavLink to="manageRestaurant" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
         Manage Restaurant
       </NavLink>
-      <button className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={()=>dispatch(setRestaurant())}>Set Restaurant</button>
+      <button className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={()=>dispatch(setRestaurant(restaurants))}>Set Restaurant</button>
     </div>
   )}
 </div>
